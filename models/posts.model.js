a00ae25cb2afe03de9e11cec622ec15764d8c5b5
gerhuete <gerhuete@gmail.com>
2020-03-19 22:55:27 -0600

    basic MVC structure with mongodb models

---
+++ models/posts.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+ 
+//Attributes of the Users object
+var postsSchema = new mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 200
+    },
+    imageUrl: { 
+    	type: String,
+    	match: /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+     },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 2000
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
+    }
+});
+ 
+mongoose.model('Posts', postsSchema);
---
+++ models/users.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+ 
+//Attributes of the Users object
+var usersSchema = new mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true },
+    name: { type: String },
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Posts' }]
+});
+ 
+mongoose.model('Users', usersSchema);
